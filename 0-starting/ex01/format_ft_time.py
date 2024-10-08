74355cc638c1b91cc61c82b83134468d365448f2
DevJ2K <ajavontheopro@gmail.com>
2024-10-08 17:38:35 +0200

    💭 : Save status...

--- 0-starting/ex01/format_ft_time.py
+++ 0-starting/ex01/format_ft_time.py
@@ -1,3 +1,19 @@
 import time as time
+from datetime import datetime
 
+curr_time_value = datetime.now()
+
+full_format = "1,666,355,857.3622" # Except
+scientific_notation_format = "1.67e+09" # Except
+current_time = "Oct 21 2022" # Except
+
+timestamp_current_time = curr_time_value.timestamp()
+# print(time.time())
+print(timestamp_current_time)
+print(f"Seconds since January 1, 1970: {full_format} or {scientific_notation_format} in scientific notation")
+
+print("EXCEPT")
+print(current_time)
+print("MINE")
+print(curr_time_value.strftime("%b %d %Y"))
 # time.
