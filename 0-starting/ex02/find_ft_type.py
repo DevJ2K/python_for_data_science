877b97d3277e30a5962a090e31f927c6d652e8ac
DevJ2K <ajavontheopro@gmail.com>
2025-05-31 19:46:14 +0200

    ✅ : Ex02

---
+++ 0-starting/ex02/find_ft_type.py
@@ -0,0 +1,12 @@
+def all_thing_is_obj(object) -> int:
+    obj_type = str(type(object))
+
+    object_type = obj_type[obj_type.find("'") + 1:obj_type.rfind("'")]
+
+    if (object_type == "str"):
+        print(f"{object} is in the kitchen : {obj_type}")
+    elif (object_type == "int"):
+        print("Type not found")
+    else:
+        print(f"{object_type.capitalize()} : {obj_type}")
+    return 42
