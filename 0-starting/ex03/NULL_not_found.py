021ca196a518388c6017ae38407d93c5f50edf3f
DevJ2K <ajavontheopro@gmail.com>
2025-05-31 20:29:26 +0200

    ✅ : Ex03

---
+++ 0-starting/ex03/NULL_not_found.py
@@ -0,0 +1,24 @@
+def NULL_not_found(object = None) -> int:
+    obj_type = str(type(object))
+    object_type = obj_type[obj_type.find("'") + 1:obj_type.rfind("'")]
+
+    if object_type != "float" and object not in [None, 0, '', False]:
+        print("Type not Found")
+        return 1
+    elif object_type == "float" and str(object) != "nan":
+        print("Type not Found")
+        return 1
+
+    match object_type:
+        case "str":
+            print("Empty:", end="")
+        case "int":
+            print("Zero: ", end="")
+        case "float":
+            print(f"Cheese: ", end="")
+        case "bool":
+            print(f"Fake: ", end="")
+        case "NoneType":
+            print("Nothing: ", end="")
+    print(f"{object} {obj_type}")
+    return 0
