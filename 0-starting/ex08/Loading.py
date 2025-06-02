11dbc235f5a22b1859873771d50a29b489c07cf7
DevJ2K <ajavontheopro@gmail.com>
2025-06-03 00:13:36 +0200

    💭 : Able to get terminal size...

--- 0-starting/ex08/Loading.py
+++ 0-starting/ex08/Loading.py
@@ -1,4 +1,11 @@
+import os
+
+
 def ft_tqdm(lst: range) -> None:
+    terminal_size = os.get_terminal_size()
     for i in lst:
         print("_", end="", flush=True)
         yield i
+
+if __name__ == "__main__":
+    print(os.get_terminal_size())
