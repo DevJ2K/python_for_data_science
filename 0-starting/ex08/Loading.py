cbfe72ed1c61e0b536f1c8b11e9d974ec3339a63
DevJ2K <ajavontheopro@gmail.com>
2025-06-08 15:40:19 +0200

    ✅ : Ex08 tqdm copy is working

--- 0-starting/ex08/Loading.py
+++ 0-starting/ex08/Loading.py
@@ -1,11 +1,33 @@
 import os
-
+import time
 
 def ft_tqdm(lst: range) -> None:
-    terminal_size = os.get_terminal_size()
+    line_clear = '\x1b[2K'
+    terminal_size = 100
+    iteration = 1
     for i in lst:
-        print("_", end="", flush=True)
+        progress_bar: str = ""
+        progression: int = int((iteration / len(lst)) * 100)
+        progress_bar += f"{progression}%|".rjust(5)
+
+        progress_bar += "█" * progression
+        progress_bar += " " * (terminal_size - progression)
+        progress_bar += f"| {iteration}/{len(lst)}"
+        # progress_bar += f" [__:__<__:__, __.__it/s]"
+
+        if iteration > 0:
+            print(line_clear, end="", flush=True)
+        print("\r" + progress_bar, end="", flush=True)
+
+        iteration += 1
         yield i
 
+# def ft_tqdm_responsive(lst: range) -> None:
+#     terminal_size = os.get_terminal_size()
+#     for i in lst:
+#         print("_", end="", flush=True)
+#         yield i
+
 if __name__ == "__main__":
+    print(len("[===============================================================>]"))
     print(os.get_terminal_size())
