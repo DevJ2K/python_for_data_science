ed82a17b6da08cbd8bf50e0d9835371a5459f88d
DevJ2K <ajavontheopro@gmail.com>
2025-06-08 16:08:44 +0200

    ✅ : Create a responsive version.

--- 0-starting/ex08/Loading.py
+++ 0-starting/ex08/Loading.py
@@ -1,5 +1,5 @@
 import os
-import time
+from typing import Generator
 
 def ft_tqdm(lst: range) -> None:
     line_clear = '\x1b[2K'
@@ -13,7 +13,6 @@ def ft_tqdm(lst: range) -> None:
         progress_bar += "█" * progression
         progress_bar += " " * (terminal_size - progression)
         progress_bar += f"| {iteration}/{len(lst)}"
-        # progress_bar += f" [__:__<__:__, __.__it/s]"
 
         if iteration > 0:
             print(line_clear, end="", flush=True)
@@ -22,12 +21,27 @@ def ft_tqdm(lst: range) -> None:
         iteration += 1
         yield i
 
-# def ft_tqdm_responsive(lst: range) -> None:
-#     terminal_size = os.get_terminal_size()
-#     for i in lst:
-#         print("_", end="", flush=True)
-#         yield i
+def ft_tqdm_responsive(lst: range) -> Generator:
+    line_clear = '\x1b[2K'
+    terminal_size = os.get_terminal_size().columns
+    iteration = 1
+    for i in lst:
+        complete_progress_bar: str = ""
+        progression: int = int((iteration / len(lst)) * 100)
+        complete_progress_bar += f"{progression}%|".rjust(5)
 
-if __name__ == "__main__":
-    print(len("[===============================================================>]"))
-    print(os.get_terminal_size())
+        after_progress_bar = f"| {iteration}/{len(lst)}"
+        after_progress_bar += f"                          "
+
+        animate_progress_bar_size = terminal_size - len(complete_progress_bar) - len(after_progress_bar)
+
+        animate_progress_bar = "█" * int((animate_progress_bar_size * progression) / 100)
+        animate_progress_bar += " " * (animate_progress_bar_size - int((animate_progress_bar_size * progression) / 100))
+
+        complete_progress_bar += animate_progress_bar + after_progress_bar
+        if iteration > 0:
+            print(line_clear, end="", flush=True)
+        print("\r" + complete_progress_bar, end="", flush=True)
+
+        iteration += 1
+        yield i
--- 0-starting/ex08/tester.py
+++ 0-starting/ex08/tester.py
@@ -1,8 +1,11 @@
 from time import sleep
 from tqdm import tqdm
-from Loading import ft_tqdm
+from Loading import ft_tqdm, ft_tqdm_responsive
 
 for elem in ft_tqdm(range(333)):
+    sleep(0.005)
+print()
+for elem in ft_tqdm_responsive(range(333)):
     sleep(0.005)
 print()
 for elem in tqdm(range(333)):
