de98b579d2aaa56befa0bf3c0316e112c7ea5f1e
DevJ2K <ajavontheopro@gmail.com>
2025-06-09 12:18:26 +0200

    💭 : Add files...

---
+++ 1-array/ex05/pimp_image.py
@@ -0,0 +1,19 @@
+from load_image import ft_load
+import numpy as np
+
+
+def ft_invert(array) -> np.ndarray:
+    #your code here
+    pass
+def ft_red(array) -> np.ndarray:
+    #your code here
+    pass
+def ft_green(array) -> np.ndarray:
+    #your code here
+    pass
+def ft_blue(array) -> np.ndarray:
+    #your code here
+    pass
+def ft_grey(array) -> np.ndarray:
+    #your code here
+    pass
