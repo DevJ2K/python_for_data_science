048a86d2b30f46cdd4fa4bb7544a037189d285f9
DevJ2K <ajavontheopro@gmail.com>
2025-06-12 21:39:43 +0200

    🗭 : Ex04...

---
+++ 3-OOP/ex04/ft_calculator.py
@@ -0,0 +1,14 @@
+class calculator:
+
+    # decorator
+    @staticmethod
+    def dotproduct(V1: list[float], V2: list[float]) -> None:...
+
+    # decorator
+    @staticmethod
+    def add_vec(V1: list[float], V2: list[float]) -> None:...
+    #your code here
+
+    @staticmethod
+    def sous_vec(V1: list[float], V2: list[float]) -> None:...
+    #your code here
