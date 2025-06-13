8dd1c966262cc54f27c0afb1468c2a0bae8391b2
DevJ2K <ajavontheopro@gmail.com>
2025-06-13 08:48:12 +0200

    ✅ : Ex04

--- 3-OOP/ex04/ft_calculator.py
+++ 3-OOP/ex04/ft_calculator.py
@@ -1,14 +1,19 @@
 class calculator:
-
-    # decorator
+    """A calculator to make operation between two vectors."""
     @staticmethod
-    def dotproduct(V1: list[float], V2: list[float]) -> None:...
-
-    # decorator
-    @staticmethod
-    def add_vec(V1: list[float], V2: list[float]) -> None:...
-    #your code here
+    def dotproduct(V1: list[float], V2: list[float]) -> None:
+        """Make the dot product between two vectors."""
+        value = sum([x1 * x2 for x1, x2 in zip(V1, V2)])
+        print(f"Dot product is: {value}")
 
     @staticmethod
-    def sous_vec(V1: list[float], V2: list[float]) -> None:...
-    #your code here
+    def add_vec(V1: list[float], V2: list[float]) -> None:
+        """Make the sum between two vectors."""
+        value = [float(x1 + x2) for x1, x2 in zip(V1, V2)]
+        print(f"Add Vector is: {value}")
+
+    @staticmethod
+    def sous_vec(V1: list[float], V2: list[float]) -> None:
+        """Make the substraction between two vectors."""
+        value = [float(x1 - x2) for x1, x2 in zip(V1, V2)]
+        print(f"Sous Vector is: {value}")
